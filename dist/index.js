06e426c7bfdf8ad32cdcbd46176cdf5f33254453
Konrad Pabjan <kopabjan@microsoft.com>
2020-04-06 19:23:13 +0200

    .

--- dist/index.js
+++ dist/index.js
@@ -5105,8 +5105,8 @@ function displayHttpDiagnostics(response) {
      HTTP Method: ${response.message.method}
      Status Code: ${response.message.statusCode}
      Status Message: ${response.message.statusMessage}
-     Raw Header Information: ${JSON.stringify(response.message.rawHeaders)}
-     Header Information: ${JSON.stringify(response.message.headers)}
+     Raw Header Information: ${JSON.stringify(response.message.rawHeaders.toString())}
+     Header Information: ${JSON.stringify(response.message.headers.toString())}
      `);
 }
 exports.displayHttpDiagnostics = displayHttpDiagnostics;
