4d002bddd13de15a7b76f2ff773128795315d3e3
Konrad Pabjan <kopabjan@microsoft.com>
2020-04-06 17:13:09 +0200

    .

--- dist/index.js
+++ dist/index.js
@@ -7234,20 +7234,19 @@ class StatusReporter {
         this.totalNumberOfFilesToProcess = fileTotal;
     }
     start() {
-        const _this = this;
         // displays information about the total upload/download status every 5 seconds
-        this.totalFileStatus = setInterval(function () {
+        this.totalFileStatus = setInterval(() => {
             // display 1 decimal place without any rounding
-            const percentage = _this.formatPercentage(_this.processedCount, _this.totalNumberOfFilesToProcess);
-            core_1.info(`Total file count: ${_this.totalNumberOfFilesToProcess} ---- Processed file #${_this.processedCount} (${percentage.slice(0, percentage.indexOf('.') + 2)}%)`);
+            const percentage = this.formatPercentage(this.processedCount, this.totalNumberOfFilesToProcess);
+            core_1.info(`Total file count: ${this.totalNumberOfFilesToProcess} ---- Processed file #${this.processedCount} (${percentage.slice(0, percentage.indexOf('.') + 2)}%)`);
         }, this.displayFrequencyInMilliseconds);
         // displays extra information about any large files that take a significant amount of time to upload or download every 1 second
-        this.largeFileStatus = setInterval(function () {
-            for (const value of Array.from(_this.largeFiles.values())) {
+        this.largeFileStatus = setInterval(() => {
+            for (const value of Array.from(this.largeFiles.values())) {
                 core_1.info(value);
             }
             // delete all entires in the map after displaying the information so it will not be displayed again unless explicitly added
-            _this.largeFiles = new Map();
+            this.largeFiles = new Map();
         }, 1000);
     }
     // if there is a large file that is being uploaded in chunks, this is used to display extra information about the status of the upload
